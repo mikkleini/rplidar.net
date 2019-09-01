eef537ff815248856844518e2ff6a9fbab669df3
mikkleini <mikk.leini@gmail.com>
2019-09-01 17:26:05 +0300

    Some updates

--- Demo/MainForm.Designer.cs
+++ Demo/MainForm.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-            this.buttonOpen = new System.Windows.Forms.ToolStripButton();
-            this.buttonClose = new System.Windows.Forms.ToolStripButton();
-            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.comboPort = new System.Windows.Forms.ToolStripComboBox();
             this.textLog = new System.Windows.Forms.TextBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
-            this.comboPort = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.comboMode = new System.Windows.Forms.ToolStripComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonOpen = new System.Windows.Forms.ToolStripButton();
+            this.buttonClose = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
@@ -46,6 +48,8 @@
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripLabel1,
             this.comboPort,
+            this.toolStripLabel2,
+            this.comboMode,
             this.buttonOpen,
             this.buttonClose});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
@@ -62,42 +66,17 @@
             this.toolStripLabel1.Size = new System.Drawing.Size(32, 20);
             this.toolStripLabel1.Text = "Port:";
             // 
-            // buttonOpen
+            // comboPort
             // 
-            this.buttonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.buttonOpen.Enabled = false;
-            this.buttonOpen.Image = ((System.Drawing.Image)(resources.GetObject("buttonOpen.Image")));
-            this.buttonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.buttonOpen.Name = "buttonOpen";
-            this.buttonOpen.Size = new System.Drawing.Size(40, 20);
-            this.buttonOpen.Text = "&Open";
-            this.buttonOpen.Click += new System.EventHandler(this.ButtonOpen_Click);
-            // 
-            // buttonClose
-            // 
-            this.buttonClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.buttonClose.Enabled = false;
-            this.buttonClose.Image = ((System.Drawing.Image)(resources.GetObject("buttonClose.Image")));
-            this.buttonClose.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(40, 20);
-            this.buttonClose.Text = "&Close";
-            this.buttonClose.Click += new System.EventHandler(this.ButtonClose_Click);
-            // 
-            // pictureBox
-            // 
-            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox.Location = new System.Drawing.Point(0, 27);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(800, 271);
-            this.pictureBox.TabIndex = 1;
-            this.pictureBox.TabStop = false;
+            this.comboPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboPort.Name = "comboPort";
+            this.comboPort.Size = new System.Drawing.Size(121, 23);
             // 
             // textLog
             // 
             this.textLog.BackColor = System.Drawing.SystemColors.Window;
             this.textLog.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.textLog.Location = new System.Drawing.Point(0, 303);
+            this.textLog.Location = new System.Drawing.Point(0, 281);
             this.textLog.Multiline = true;
             this.textLog.Name = "textLog";
             this.textLog.ReadOnly = true;
@@ -108,17 +87,59 @@
             // splitter1
             // 
             this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.splitter1.Location = new System.Drawing.Point(0, 298);
+            this.splitter1.Location = new System.Drawing.Point(0, 276);
             this.splitter1.Name = "splitter1";
             this.splitter1.Size = new System.Drawing.Size(800, 5);
             this.splitter1.TabIndex = 3;
             this.splitter1.TabStop = false;
             // 
-            // comboPort
+            // toolStripLabel2
             // 
-            this.comboPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboPort.Name = "comboPort";
-            this.comboPort.Size = new System.Drawing.Size(121, 23);
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(41, 20);
+            this.toolStripLabel2.Text = "Mode:";
+            // 
+            // comboMode
+            // 
+            this.comboMode.Name = "comboMode";
+            this.comboMode.Size = new System.Drawing.Size(121, 23);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 249);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Enabled = false;
+            this.buttonOpen.Image = global::Demo.Properties.Resources.PlayHS;
+            this.buttonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(56, 20);
+            this.buttonOpen.Text = "&Open";
+            this.buttonOpen.Click += new System.EventHandler(this.ButtonOpen_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Enabled = false;
+            this.buttonClose.Image = global::Demo.Properties.Resources.StopHS;
+            this.buttonClose.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(56, 20);
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
             // 
             // MainForm
             // 
@@ -129,6 +150,7 @@
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.textLog);
             this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
             this.Name = "MainForm";
             this.Text = "RPLidar.NET demo";
             this.toolStrip1.ResumeLayout(false);
@@ -149,6 +171,9 @@
         private System.Windows.Forms.TextBox textLog;
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.ToolStripComboBox comboPort;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripComboBox comboMode;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- Demo/Properties/Resources.Designer.cs
+++ Demo/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace Demo.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PlayHS {
+            get {
+                object obj = ResourceManager.GetObject("PlayHS", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap StopHS {
+            get {
+                object obj = ResourceManager.GetObject("StopHS", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
